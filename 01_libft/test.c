d0e86ba7c905d9f4e9f65fcae67d022974017c17
zy42 <zengying1942@gmail.com>
2023-09-09 01:52:17 +0800

    Edited 3 functions
    ls

--- 01_libft/test.c
+++ 01_libft/test.c
@@ -3,15 +3,36 @@
 #include <stdio.h>
 
 int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 
 int	main()
 {
 	char *testcases = "AaBZz01 .";
+	printf("ft_isalpha: \n");
 	while (*testcases)
 	{
 		char c = *testcases;
-		char *result = (isalpha(c) == ft_isalpha(c)) ? "PASSED": "FAILED";
+		char *result = ( (isalpha(c) > 0) == (ft_isalpha(c) > 0)) ? "PASSED": "FAILED";
 		printf("Input: %d; Expect: %d; Result: %d; Test: %s\n", c, isalpha(c), ft_isalpha(c), result);
+		result = (isdigit(c) == ft_isdigit(c)) ? "PASSED": "FAILED";
 		testcases++;
 	}
+	printf("ft_isdigit: \n");
+	testcases = "1s7F. ";
+	while (*testcases)
+        {
+                char c = *testcases;
+                char *result = ((isdigit(c) > 0) == (ft_isdigit(c) > 0)) ? "PASSED": "FAILED";
+                printf("Input: %d; Expect: %d; Result: %d; Test: %s\n", c, isdigit(c), ft_isdigit(c), result);                testcases++;
+        }
+
+	printf("ft_isalnum: \n");
+        testcases = "1s7F.P ";
+        while (*testcases)
+        {
+                char c = *testcases;
+                char *result = ((isalnum(c) > 0) == (ft_isalnum(c) > 0)) ? "PASSED": "FAILED";
+                printf("Input: %d; Expect: %d; Result: %d; Test: %s\n", c, isalnum(c), ft_isalnum(c), result);                testcases++;
+        }
 }
