d66e9c52bf3dddf6e3c21b9fb552a7d47f2a2280
zy42 <zengying1942@gmail.com>
2023-09-25 15:56:21 +0800

    edit atoi

--- 01_libft/ft_itoa.c
+++ 01_libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: yzeng <yzeng@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 06:03:19 by yzeng             #+#    #+#             */
-/*   Updated: 2023/09/24 07:09:31 by yzeng            ###   ########.fr       */
+/*   Updated: 2023/09/25 15:42:33 by zengying         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,42 +15,54 @@
 ** representing the integer received as an argument.
 ** Negative numbers must be handled.
 */
-void	update(char *str,int num, int index)
+void	update(char *str, int num, int *p_index)
 {
+	if (num < 0)
+		num *= -1;
 	if (num > 10)
 	{
-		update(str,num / 10,index);
-                index++;
-        	update(str,num % 10, index);
+		update(str, num / 10, p_index);
+		*p_index += 1;
+		update(str, num % 10, p_index);
 	}
 	else
-		update(str,num % 10, index);
+		str[*p_index] = num % 10 + '0';
 }
 
-int	int_len(int n)
-{
-	int	i;
+/* int_min > int_max
+ * control for int_min (–2147483648)
+ * if not, leads to overflow when int_min * -1
+ */
 
-	i  = 0;
-	if (n < 0)
-		n *= -1;
-	while (n / 10)
-	{
-		i++;
-	}
-	return (i);
-}
 char	*ft_itoa(int n)
 {
-	int	index = 0;
+	int		index;
+	int		*p_index;
 	char	*str;
 
-	//printf("%i", int_len(n));
+	p_index = &index;
 	str = (char *) malloc (sizeof(char) * (12));
-	update(str, n, 0);
+	if (n < 0)
+	{
+		if (n == -2147483648)
+		{
+			str = "–2147483648";
+			return (str);
+		}
+		index = 1;
+		str[0] = '-';
+		update(str, n * -1, p_index);
+	}
+	else
+	{
+		index = 0;
+		update(str, n, p_index);
+	}
+	str[*p_index + 1] = '\0';
+	return (str);
 }
-
+/*
 int	main()
 {
-	printf("%s\n", ft_itoa(12));
-}
+	printf("%s\n", ft_itoa(-15234));
+}*/
